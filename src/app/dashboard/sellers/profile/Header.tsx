9dbd75b840d27cf911d8dcb91a18e24e1fb9e8f6
mahadi hasan <mahadi.dev@outlook.com>
2024-06-26 16:10:46 +0600

    feat: seller page back history

--- src/app/dashboard/sellers/profile/Header.tsx
+++ src/app/dashboard/sellers/profile/Header.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Header() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
   const name = searchParams.get("name");
+  const router = useRouter();
 
   return (
     <div className="text- 1font-medium flex w-full items-center justify-between bg-blue-400 p-4 font-bold text-white">
       <div className="flex gap-4">
-        <Link href="/dashboard/sellers">Home</Link>
+        <a onClick={() => router.back()} className="cursor-pointer">
+          Home
+        </a>
         <Link href={`/dashboard/sellers/profile?id=${id}&name=${name}`}>
           Profile
         </Link>
