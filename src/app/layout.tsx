0cdd0493694d9e70bda973441f2d2f72fae92927
mahadi hasan <mahadi.dev@outlook.com>
2024-09-21 14:53:54 +0600

    changed app title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "China Goods",
+  title: "China Cup",
   description: "An e-commerce site",
 };
 
