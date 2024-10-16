6c6e0dfa163ace0d0209942681ac5738db2fe7df
mahadi-hasan <init.mahadi@gmail.com>
2024-10-17 02:17:02 +0600

    decrease progress bar height

--- src/app/progress-provider.tsx
+++ src/app/progress-provider.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 const ProgressProvider = () => {
-  return <ProgressBar height="8px" color="#FC771E" shallowRouting />;
+  return <ProgressBar height="5px" color="#FC771E" shallowRouting />;
 };
 
 export default ProgressProvider;
